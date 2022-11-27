01bc8c73e23d262efeaa80726b4b04480400bfee
abdo99ab <fa_hammia@esi.dz>
2022-11-27 13:43:49 +0100

    Added mastermind urls using nested routers, game, guesses and statuses

---
+++ django_rest/mastermind/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework_nested import routers
+from mastermind.views import *
+
+router = routers.DefaultRouter()
+router.register(r'games', GameView)
+game_router = routers.NestedDefaultRouter(router, r'games', lookup='game')
+game_router.register(r'guesses', GuessView, basename='game-guesses')
+
+urlpatterns = [
+    path('', include(router.urls),name="game"),
+    path('', include(game_router.urls),name="game-guesses"),
+    path('statuses/', StatusView.as_view(), name="statuses"),
+]
