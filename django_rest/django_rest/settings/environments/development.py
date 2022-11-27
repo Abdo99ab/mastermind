97e408b7e8f549a9ce487897700bdf56c4e20654
abdo99ab <fa_hammia@esi.dz>
2022-11-27 15:21:20 +0100

    Removed SECRET_KEY from development.py and added .env to gitignore

--- django_rest/django_rest/settings/environments/development.py
+++ django_rest/django_rest/settings/environments/development.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 MY_ENV = 'dev'
 DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY') or os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = ["localhost","127.0.0.1"]
